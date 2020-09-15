6282427a7f6469ebff39a1b590cd35180a0192f7
svdoit <doemily@naver.com>
2020-09-16 02:41:36 +0900

    [Social Login] Edited Blog>urls.py

--- Project_DjangoLL/Blog/urls.py
+++ Project_DjangoLL/Blog/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -11,6 +11,8 @@ urlpatterns = [
     path('blog/create', views.blogCreateView, name="blogCreateFn"),
     path('update/<int:blog_id>/', views.update, name = "update"),
     path('delete/<int:blog_id>/', views.delete, name = "delete"),
+    
+    path('account/', include('allauth.urls')),
 ]
 
 urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
